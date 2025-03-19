ee5c191146b1089660bbe9567a48f44cde632eaf
angle-zhang <angelazhang1220@gmail.com>
2025-03-18 18:32:59 -0700

    pull in parcel data

--- R-food-access-lh/0_Libraries.R
+++ R-food-access-lh/0_Libraries.R
@@ -4,6 +4,9 @@ library(tidyverse)
 library(CDCPLACES)
 library(tmap)
 library(crsuggest)
+library(sf)
+library(tigris)
+library(dplyr)
 
 # 1
 library(centr)
@@ -14,6 +17,6 @@ source('C:/Users/angie/OneDrive/Desktop/data-analysis/0_helper-functions/get-foo
 source('C:/Users/angie/OneDrive/Desktop/data-analysis/0_helper-functions/get-la-county-admin-data.R')
 source('C:/Users/angie/OneDrive/Desktop/data-analysis/0_helper-functions/get-health-data.R')
 
-library(reticulate)
-py_run_file('C:/Users/angie/OneDrive/Desktop/data-analysis/0_helper-functions/get_osm_data.py')
+#library(reticulate)
+#py_run_file('C:/Users/angie/OneDrive/Desktop/data-analysis/0_helper-functions/get_osm_data.py')
 
--- R-food-access-lh/1_data_collection.R
+++ R-food-access-lh/1_data_collection.R
@@ -1,37 +1,73 @@
 download_foodins_lacounty_ssi()
-download_census_tracts()
-download_census_blocks()
+download_census_tracts(state="CA", county="Los Angeles", year=2020)
+download_census_blocks(state="CA", county="Los Angeles", year=2020)
+
+processed_path <- "C:/Users/angie/OneDrive/Desktop/data-analysis/0_shared-data/processed/"
 
 # ------ LOAD BOUNDARIES AND CRS ------ #
-la_county <- get_county_boundary()
-proj_crs = as.integer(suggest_crs(la_county)$crs_code[1])
-la_ct <- get_census_tracts(proj_crs)
-la_cb <- get_census_blocks(proj_crs)
+# TODO get edges outside LA County
+# get suggested CRS
+
+proj_crs = as.integer(suggest_crs(get_county_boundary())$crs_code[1])
+
+# convert to suggested crs
+lac_boundary <- st_transform(get_county_boundary(), proj_crs)
+st_crs(lac_boundary, parameters = TRUE)$units_gdal # check units
+
+lac_buffer <- st_buffer(lac_boundary, 5280*15) # get buffer in 15 mile zone
+
+#la_hh <- get_lac_households(proj_crs)
+## clip to boundary, right now buffer area not needed 
+la_ct <- get_census_tracts(proj_crs, state="CA", year=2020, county="Los Angeles")
+la_cb <- get_census_blocks(proj_crs, state="CA", year=2020, county="Los Angeles")
 
 # get geometry type of la_cb
 print(unique(st_geometry_type(la_cb)))
 unique(st_is_valid(la_cb, reason=T))
 
-
-# ------ GET POPULATION WEIGHED CENTROID OF CTs ------ #
+# ------ GET CENTROIDS OF CTs ------ #
 # gets centroid of census blocks then calculates the population weighted centroid of a census tract based on those
 # for centroids that lie outside a census block, st_point_on_surface is used to get a point within the polygon
 la_ctcent <- get_pop_weighted_centroid(la_cb, 'TRACTCE20', 'POP20') %>% 
   st_transform(proj_crs)
 
-# merge ct centroids with rest of data
-la_ctcent_dat <- la_ct %>%
+# merge ct weighted centroids with rest of census tract data
+la_ct_wtcent_dat <- la_ct %>%
   st_drop_geometry() %>%
   left_join(la_ctcent, by=c('TRACTCE'='TRACTCE20')) %>%
   st_as_sf()
 
+# get unweighted centroids of census tracts
+la_ctcent_dat <- la_ct %>%
+  st_centroid() %>%
+  st_transform(proj_crs)
+
+write_sf(la_ct_wtcent_dat, paste0(processed_path, "LAC_origins/la_ct_wtcent_dat3182025.gpkg"))
+
+write_sf(la_ctcent_dat, paste0(processed_path, "LAC_origins/la_ctcent_dat3182025.gpkg"))
+
+get_lac_weight_centroids <- function() {
+  return(st_read(paste0(processed_path, "LAC_origins/la_ct_wtcent_dat3182025.gpkg")))
+}
+get_lac_centroids <- function() {
+  return(st_read(paste0(processed_path, "LAC_origins/la_ctcent_dat3182025.gpkg")))
+}
+
+la_ct_wtcent_dat <- get_lac_weight_centroids()
+la_ctcent_dat <- get_lac_centroids()
+
+rm(la_ctcent_dat, la_ct_wtcent_dat, la_ctcent, la_ct, la_cb)
 
 unique(st_geometry_type(la_ctcent_dat))
 st_crs(la_ctcent_dat)
 
+# get household points from parcel data
+la_hh <- get_lac_households(proj_crs)
+# get sample for mapping
+la_hh_sample <- la_hh[sample(nrow(la_hh), 5000), ]
 # ------ LOAD SNAP POI DATA ------ #
 # Load SNAP historical data for the year 2021
-snap_historical <- get_snap_historical(years = 2021, proj_crs = st_crs(la_county))
+snap_historical <- get_snap_historical(years = 2021, proj_crs = st_crs(lac_boundary))
 
 # ------ LOAD FOOD INSPECTION POI DATA ------ #
 # Load LA County food inspection data (2021-2024)
