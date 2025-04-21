0ba8dc48a710cdeb720aedfa9241ef25c916a772
angle-zhang <angelazhang1220@gmail.com>
2025-04-21 16:47:22 -0700

    adding restaurant data

--- R-food-access-lh/1_data_cleaning.R
+++ R-food-access-lh/1_data_cleaning.R
@@ -5,42 +5,50 @@ source("0_Libraries.R")
 foodinsp_21_24 <- get_foodinsp_lacounty()
 
 # Load LA County food inspection data (2023-2024) from SSI
-foodinsp23_24_SSI <- get_foodins_lacounty_ssi(proj_crs)
-foodmarket_23_DA <- get_retail_food_markets_LB_PAS(proj_crs)
+food_23_24_SSI <- get_foodins_lacounty_ssi(proj_crs)
+food_23_DA <- get_retail_food_LB_PAS(proj_crs)
 
 # remove duplicates with facility ID, make sure to get the most recent status open vs closed
-foodinsp23_24_SSIclean <- foodinsp23_24_SSI %>% 
-  mutate(SOURCE=factor(SOURCE, ordered=T, levels=c("Dec_2023", "March_2024","June_2024", "Dec_2024"))) %>%
-  group_by(FACILITY_ID) %>% 
-  filter(SOURCE == max(SOURCE)) %>% 
-  ungroup() %>%
-  mutate(SOURCE="food_inspection") %>%
-  mutate(
-    small = ifelse(grepl("1-1,999", PE_DESCRIPTION), 1, 0),
-    large = ifelse(grepl("2,000", PE_DESCRIPTION), 1, 0)
-  ) %>%
-  st_transform(4326) %>% # OSM data is in 4326
-  mutate(lon = GEOCODE_LONGITUDE, lat = GEOCODE_LATITUDE) %>%
-  mutate(count=1) %>%
-  select(OBJECTID, lon, lat, count, small, large, MATCH_ADDR, FACILITY_NAME, SOURCE)
+clean_food_data <- function(type="markets") {
+  food_23_24_SSIclean <- food_23_24_SSI %>% 
+    mutate(SOURCE=factor(SOURCE, ordered=T, levels=c("Dec_2023", "March_2024","June_2024", "Dec_2024"))) %>%
+    group_by(FACILITY_ID) %>% 
+    filter(SOURCE == max(SOURCE)) %>% # get the one with largest value
+    ungroup() %>%
+    mutate(SOURCE="food_inspection") %>%
+    mutate(
+      small = ifelse(grepl("1-1,999", PE_DESCRIPTION), 1, 0),
+      large = ifelse(grepl("2,000", PE_DESCRIPTION), 1, 0)
+    ) %>%
+    st_transform(4326) %>% # OSM data is in 4326
+    mutate(lon = GEOCODE_LONGITUDE, lat = GEOCODE_LATITUDE) %>%
+    mutate(count=1) %>%
+    select(OBJECTID, lon, lat, count, small, large, MATCH_ADDR, FACILITY_NAME, SOURCE, TYPE) 
+  print(paste("Number of", type, "in food inspection:", nrow(food_23_24_SSIclean %>% filter(TYPE==type))))
+  
+  # clean data axle market data from 2023
+  food_23_DA_clean <- food_23_DA %>%
+    mutate(SOURCE="data_axle") %>%
+    rename(FACILITY_NAME = COMPANY_NAME) %>%
+    st_transform(4326) %>% # OSM data is in 4326
+    mutate(lon = st_coordinates(.)[,1], lat = st_coordinates(.)[,2]) %>%
+    mutate(count=1) %>%
+    select(OBJECTID, lon, lat, count, MATCH_ADDR, FACILITY_NAME, SOURCE, TYPE)
+  # print number of type in DA
+  print(paste("Number of", type, "in data axle:", nrow(food_23_DA_clean %>% filter(TYPE==type))))
+  
+  food_merged <- bind_rows(food_23_24_SSIclean, food_23_DA_clean) %>%
+    filter(TYPE==type) %>%
+    mutate(SOURCE_OBJECTID=OBJECTID, id=row_number()) 
+  
+  return(food_merged)
+}
 
-# clean data axle market data from 2023
-foodmarket_23_DA_clean <- foodmarket_23_DA %>%
-  mutate(SOURCE="data_axle") %>%
-  rename(FACILITY_NAME = COMPANY_NAME) %>%
-  st_transform(4326) %>% # OSM data is in 4326
-  mutate(lon = st_coordinates(.)[,1], lat = st_coordinates(.)[,2]) %>%
-  mutate(count=1) %>%
-  select(OBJECTID, lon, lat, count, MATCH_ADDR, FACILITY_NAME, SOURCE)
-         
-         
-foodmarket_merged <- bind_rows(foodinsp23_24_SSIclean, foodmarket_23_DA_clean) %>%
-  mutate(SOURCE_OBJECTID=OBJECTID, id=row_number())
+markets <- clean_food_data(type="markets")
+restaurants <- clean_food_data(type="res")
 
-
+# TODO  cleaning names 
 # find all names with # followed by number #9357
-foodmarket <- foodmarket_merged %>%
-  filter(grepl("#\\d+", FACILITY_NAME)) 
 
 length(unique(foodmarket_merged$FACILITY_NAME)) # get all unique names with # followed by number
 
@@ -115,11 +123,6 @@ foodmarket_cleaned <- foodmarket_merged %>%
 
 print(paste("Unique names found after removing numbers with # sign before them:",  
             length(unique(foodmarket_merged$FACILITY_NAME))- length(unique(foodmarket_cleaned$FACILITY_NAME)))) # get all unique names with # followed by number
-
-
-
-
-
 
 
 # TODO identify columns in original data that may have info about owners (will give chain status if many of same owner)
