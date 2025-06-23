c9622f7d1f157705846cc1f829269900b41839e7
angle-zhang <angelazhang1220@gmail.com>
2025-06-23 15:42:11 -0700

    write

--- R-food-access-lh/1_data_cleaning.R
+++ R-food-access-lh/1_data_cleaning.R
@@ -84,83 +84,85 @@ foodpoi <- temp %>%
   as.data.frame() %>%
   select(-c(6:8)) 
 
+# write foodpoi to file
+write.csv(foodpoi, paste0(processed_path, "foodpoi.csv"), row.names = FALSE)
 
 #'*sensitivity analysis: inspect to see if chains are consistently coded *
 # find that the chains are consistently coded 
 # 697 rows for NAICS and name pairs that had a count >5
 # only 1 inconsistency was found "albertson's delicatssen" was coded as restaurant and supermarket
-# kept as is since there may be reason for coding this way
-chains <- temp[, .(count = .N), by = .(`zhang-2025`, code, COMPANY)][count>5]
-
-
-
-# TODO clip to a buffer
-
-
-foodmarket_names <- foodmarket %>%
-  mutate(FACILITY_NAME = str_replace_all(FACILITY_NAME, "#\\d+", "")) 
-
-unique(foodmarket_names$FACILITY_NAME) 
-# repalce @ with AT
-foodmarket_cleaned <- foodmarket_merged %>%
-  mutate(FACILITY_NAME = trimws(FACILITY_NAME)) %>%
-  mutate(FACILITY_NAME = str_replace_all(FACILITY_NAME, "#\\d+", "")) %>%
-  mutate(FACILITY_NAME = str_replace_all(FACILITY_NAME, "# \\d+", "")) %>%
-  mutate(FACILITY_NAME = gsub("@", "AT", FACILITY_NAME)) %>%
-  mutate(FACILITY_NAME = trimws(FACILITY_NAME)) 
-# remove trailing spaces
-
-# merge with chain names
-foodmarket_cleaned1 <- foodmarket_cleaned %>%
-  fuzzyjoin::stringdist_left_join(chains2, by=c("FACILITY_NAME"="COMPANY"), max_dist=1) %>%
-  mutate(chain = ifelse(sic_codes!="NULL", TRUE, FALSE)) 
-
-# TODO remove duplicates?
-
-length(unique(chains2$COMPANY))
-nrow(foodmarket_cleaned1 %>% filter(chain))
-
-temp <- chains1 %>%
-  count(COMPANY)
-
-print(paste("Unique names found after removing numbers with # sign before them:",  
-            length(unique(foodmarket_merged$FACILITY_NAME))- length(unique(foodmarket_cleaned$FACILITY_NAME)))) # get all unique names with # followed by number
-
-#'* DON'T DELETE METHOD FOR OBTAINING CHAINS AND ASSOCIATED SIC CODES *---------------------------------------------------
-chains <- poi_da %>%
-  group_by(COMPANY) %>%
-  summarise(count = n()) %>%
-  arrange(desc(count)) %>%
-  filter(count>=5)
-
-## for each name, get the most common primary sic code
-chains_sic <- poi_da %>%
-  filter(COMPANY %in% chains$COMPANY) %>%
-  group_by(COMPANY, PRIMARY.SIC.CODE) %>%
-  summarize(count=n()) %>%
-  slice_max(count)
-
-chains1 <- chains %>% merge(chains_sic, by="COMPANY") %>%
-  select(COMPANY, PRIMARY.SIC.CODE, count.x) %>%
-  mutate(sic4 = substr(PRIMARY.SIC.CODE, 1, 4)) %>%
-  filter(sic4 %in% sic_list4$.) %>% # filter for the ones we are interested in
-  group_by(COMPANY) %>%
-  summarize(sic_codes=list(PRIMARY.SIC.CODE)) 
-
-# put word market after SIC codes starting with 5411 unless they already have "market" in the
-chains2 <- chains1 %>%
-  mutate(COMPANY=ifelse(grepl("MARKETPLACE", COMPANY), gsub(" MARKETPLACE", "", COMPANY), COMPANY)) %>%
-  # TODO do something about costco
-  rowwise() %>%
-  mutate(
-    COMPANY = if (any(str_starts(as.character(sic_codes), "5411")) && !str_detect(str_to_lower(COMPANY), "market")) 
-      paste0(COMPANY, " MARKET") 
-    else COMPANY
-  ) %>%
-  ungroup() %>%
-  rbind(chains1) %>%
-  unique()
-
-
-# TODO complex strat: 
+#' # kept as is since there may be reason for coding this way
+#' chains <- temp[, .(count = .N), by = .(`zhang-2025`, code, COMPANY)][count>5]
+#' 
+#' 
+#' 
+#' # TODO clip to a buffer
+#' 
+#' 
+#' foodmarket_names <- foodmarket %>%
+#'   mutate(FACILITY_NAME = str_replace_all(FACILITY_NAME, "#\\d+", "")) 
+#' 
+#' unique(foodmarket_names$FACILITY_NAME) 
+#' # repalce @ with AT
+#' foodmarket_cleaned <- foodmarket_merged %>%
+#'   mutate(FACILITY_NAME = trimws(FACILITY_NAME)) %>%
+#'   mutate(FACILITY_NAME = str_replace_all(FACILITY_NAME, "#\\d+", "")) %>%
+#'   mutate(FACILITY_NAME = str_replace_all(FACILITY_NAME, "# \\d+", "")) %>%
+#'   mutate(FACILITY_NAME = gsub("@", "AT", FACILITY_NAME)) %>%
+#'   mutate(FACILITY_NAME = trimws(FACILITY_NAME)) 
+#' # remove trailing spaces
+#' 
+#' # merge with chain names
+#' foodmarket_cleaned1 <- foodmarket_cleaned %>%
+#'   fuzzyjoin::stringdist_left_join(chains2, by=c("FACILITY_NAME"="COMPANY"), max_dist=1) %>%
+#'   mutate(chain = ifelse(sic_codes!="NULL", TRUE, FALSE)) 
+#' 
+#' # TODO remove duplicates?
+#' 
+#' length(unique(chains2$COMPANY))
+#' nrow(foodmarket_cleaned1 %>% filter(chain))
+#' 
+#' temp <- chains1 %>%
+#'   count(COMPANY)
+#' 
+#' print(paste("Unique names found after removing numbers with # sign before them:",  
+#'             length(unique(foodmarket_merged$FACILITY_NAME))- length(unique(foodmarket_cleaned$FACILITY_NAME)))) # get all unique names with # followed by number
+#' 
+#' #'* DON'T DELETE METHOD FOR OBTAINING CHAINS AND ASSOCIATED SIC CODES *---------------------------------------------------
+#' chains <- poi_da %>%
+#'   group_by(COMPANY) %>%
+#'   summarise(count = n()) %>%
+#'   arrange(desc(count)) %>%
+#'   filter(count>=5)
+#' 
+#' ## for each name, get the most common primary sic code
+#' chains_sic <- poi_da %>%
+#'   filter(COMPANY %in% chains$COMPANY) %>%
+#'   group_by(COMPANY, PRIMARY.SIC.CODE) %>%
+#'   summarize(count=n()) %>%
+#'   slice_max(count)
+#' 
+#' chains1 <- chains %>% merge(chains_sic, by="COMPANY") %>%
+#'   select(COMPANY, PRIMARY.SIC.CODE, count.x) %>%
+#'   mutate(sic4 = substr(PRIMARY.SIC.CODE, 1, 4)) %>%
+#'   filter(sic4 %in% sic_list4$.) %>% # filter for the ones we are interested in
+#'   group_by(COMPANY) %>%
+#'   summarize(sic_codes=list(PRIMARY.SIC.CODE)) 
+#' 
+#' # put word market after SIC codes starting with 5411 unless they already have "market" in the
+#' chains2 <- chains1 %>%
+#'   mutate(COMPANY=ifelse(grepl("MARKETPLACE", COMPANY), gsub(" MARKETPLACE", "", COMPANY), COMPANY)) %>%
+#'   # TODO do something about costco
+#'   rowwise() %>%
+#'   mutate(
+#'     COMPANY = if (any(str_starts(as.character(sic_codes), "5411")) && !str_detect(str_to_lower(COMPANY), "market")) 
+#'       paste0(COMPANY, " MARKET") 
+#'     else COMPANY
+#'   ) %>%
+#'   ungroup() %>%
+#'   rbind(chains1) %>%
+#'   unique()
+#' 
+#' 
+#' # TODO complex strat: 
 
