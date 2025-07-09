ab409b8897f810c85bc7f0ac84ff0d8bf41a8998
angle-zhang <36026781+angle-zhang@users.noreply.github.com>
2025-07-09 13:15:17 -0700

    Update 2_gen_measures.R

--- R-food-access-lh/2_gen_measures.R
+++ R-food-access-lh/2_gen_measures.R
@@ -1,18 +1,18 @@
 source("0_Libraries.R")
 
 # ------ CALCULATE PROXIMITY MEASURES ------ #
-proximity_measure <- function (pop_cent, poi, mode='classic') {
+# proximity_measure <- function (pop_cent, poi, mode='classic') {
   # Calculate proximity measures
-  nearest <- st_nearest_feature(pop_cent, poi, check_crs = TRUE)
-  if(mode == 'classic') {
-    dist <- st_distance(pop_cent, poi[nearest,], by_element = TRUE)
-  } else if(mode == 'network') {
+# nearest <- st_nearest_feature(pop_cent, poi, check_crs = TRUE)
+#  if(mode == 'classic') {
+#    dist <- st_distance(pop_cent, poi[nearest,], by_element = TRUE)
+#  } else if(mode == 'network') {
     # TODO calculate distance by street network
-  }
+#  }
   #assign row number of nearest poi to population data
-  pop_cent$nearest_poi <- nearest
-  pop_cent$nearest_dist <- dist
-}
+#  pop_cent$nearest_poi <- nearest
+#  pop_cent$nearest_dist <- dist
+#}
 
 # ------ LOAD ORIGINS ------ #
 # get household points from parcel data
